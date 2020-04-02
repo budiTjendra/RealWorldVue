06f9a0a2a2f952f594ad327e5d8c1992f1be1ef9
budi-injo <budi.injo@telepathy.ai>
2020-04-02 21:58:48 +0800

    minification using terser plugin

--- real-world-vue/vue.config.js
+++ real-world-vue/vue.config.js
@@ -1,6 +1,25 @@
 const CompressionPlugin = require('compression-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
   configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          sourceMap: true,
+          terserOptions: {
+            mangle: true,
+            keep_fnames: true,
+            output: {
+              ascii_only: true,
+              beautify: false
+            }
+          },
+          extractComments: true
+        })
+      ]
+    },
     plugins: [
       new CompressionPlugin({
         filename: '[path].br[query]',
