eaf7cc55eb909496e2ef11d4a6db36ac68180347
budi-injo <budi.injo@telepathy.ai>
2020-04-03 17:27:50 +0800

    move router-link to EventCard

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,19 +1,50 @@
 <template>
-  <div>{{ title }}</div>
+  <router-link :to="{ name: 'Show', params: { id: 1 } }">
+    <div class="event-card -shadow">
+      <span class="eyebrow">{{ event.date }} on {{ event.time }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: 'this is event card'
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19',
+        time: '16:00',
+        attendees: [
+          { id: 'A1', name: 'Budi' },
+          { id: 'A2', name: 'Maria' }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-div {
-  color: brown;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
--- real-world-vue/src/views/List.vue
+++ real-world-vue/src/views/List.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <h1>This is list page</h1>
-
-    <EventCard v-for="index in 3" :key="index" />
-
-    <router-link :to="{ name: 'Show', params: { id: 1 } }"
-      >Show Event #1</router-link
-    >|
+    <EventCard />
   </div>
 </template>
 
