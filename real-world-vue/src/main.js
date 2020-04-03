f20c65a102d94b060c07526dca568ddf05e42711
budi-injo <budi.injo@telepathy.ai>
2020-04-03 17:49:35 +0800

    create Global Component

--- real-world-vue/src/main.js
+++ real-world-vue/src/main.js
@@ -1,12 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue'
+Vue.component('BaseIcon', BaseIcon)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
