e24159066fbe30967da559164ee807dcb1659958
budi-injo <budi.injo@telepathy.ai>
2020-04-03 16:57:57 +0800

    create event card and show in list

--- real-world-vue/src/views/List.vue
+++ real-world-vue/src/views/List.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>This is list page</h1>
+
+    <EventCard v-for="index in 3" :key="index" />
+
     <router-link :to="{ name: 'Show', params: { id: 1 } }"
       >Show Event #1</router-link
     >|
@@ -8,7 +11,17 @@
 </template>
 
 <script>
-export default {}
+import EventCard from '@/components/EventCard'
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      numbers: 1
+    }
+  }
+}
 </script>
 
 <style></style>
