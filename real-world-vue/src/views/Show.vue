5828865c174d7bc2df2067b5ced4f796414456f1
budi-injo <budi.injo@telepathy.ai>
2020-04-03 23:26:00 +0800

    implement api call getEventId in Show

--- real-world-vue/src/views/Show.vue
+++ real-world-vue/src/views/Show.vue
@@ -3,14 +3,29 @@
     <h1>
       Showing Event
       <br />
-      #{{ id }}
+      #{{ event.title }}
     </h1>
   </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    this.getEvent(this.id)
+  },
+  methods: {
+    async getEvent(eventId) {
+      const response = await EventService.getEvent(eventId) // <--- Send the prop id to our EventService
+      this.event = await response.data
+    }
+  }
 }
 </script>
 
