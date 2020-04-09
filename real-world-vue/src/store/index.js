32aa29b1b3f1e362cb6186b021d297426448fe4f
budi-injo <budi.injo@telepathy.ai>
2020-04-09 22:04:14 +0800

    add mutation and action into Vuex Store

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import EventService from '@/services/EventService.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -23,8 +23,17 @@ export default new Vuex.Store({
     ],
     events: [{ id: 1 }, { id: 2 }]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+      commit('ADD_EVENT', event)
+    }
+  },
   modules: {},
   getters: {
     catLength: state => {
